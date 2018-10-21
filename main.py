d224fdb77697c9ce2ee9a665757f52675a132a1f
Wamae <wamaebenson06@gmail.com>
2018-10-21 23:15:41 +0300

    Memoized

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 
-def fib(n):
+def fib(n,memo):
 	if(n<=0):
 		return 0
 	elif(n==1):
 		return 1
-	else:
-		return fib(n-1)+fib(n-2)
+	elif(n in memo):
+		return memo[n]
 
+	memo[n] = fib(n-1,memo)+fib(n-2,memo)
+	return memo[n]
+
+memo = {}
 for i in range(0,11):
-	print(fib(i))
+	print(fib(i,memo))
